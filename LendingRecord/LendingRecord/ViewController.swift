6b13eae5d85576fb3ca7f7f8b451a6053d926c86
Durvesh Navande <durveshnavande7@gmail.com>
2018-03-15 05:01:48 +0530

    ui

---
+++ LendingRecord/LendingRecord/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  LendingRecord
+//
+//  Created by Durvesh Navande on 13/03/18.
+//  Copyright © 2018 reloaded. All rights reserved.
+//
+
+import UIKit
+import RealmSwift 
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ LendingRecord/LendingRecord/cell.swift
@@ -0,0 +1,21 @@
+//
+//  cell.swift
+//  LendingRecord
+//
+//  Created by Durvesh Navande on 13/03/18.
+//  Copyright © 2018 reloaded. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class cell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+
+}
