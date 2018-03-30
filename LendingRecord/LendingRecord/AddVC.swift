c478f0bd5fc5d26628e65039583d084839b69994
Durvesh Navande <durveshnavande7@gmail.com>
2018-03-31 00:00:46 +0530

    Initial commit

--- LendingRecord/LendingRecord/AddVC.swift
+++ LendingRecord/LendingRecord/AddVC.swift
@@ -9,27 +9,30 @@
 import UIKit
 
 class AddVC: UIViewController {
-
+    
+    
+    @IBOutlet weak var myImage: UIImageView!
+    @IBOutlet weak var givePrice: UITextField!
+    @IBOutlet weak var takePrice: UITextField!
+    @IBOutlet weak var giveNote: UITextField!
+    @IBOutlet weak var takeNote: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func saveTapped(_ sender: Any) {
+        
+        var newTrans = Trans()
+        
+        if let price = takePrice.text{
+            newTrans.price = price
+        }
+        print(newTrans.price)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
---
+++ LendingRecord/LendingRecord/Trans.swift
@@ -0,0 +1,20 @@
+//
+//  Trans.swift
+//  LendingRecord
+//
+//  Created by Durvesh Navande on 30/03/18.
+//  Copyright © 2018 reloaded. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Trans: Object {
+    
+    @objc dynamic var price = ""
+    
+}
+
+
+
+
